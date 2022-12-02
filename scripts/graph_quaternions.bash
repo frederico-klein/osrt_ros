0c0b72bf61bfd1e428b7088801d985a2eeec5532
frederico-klein <frekle@kth.se>
2022-12-02 13:21:00 +0100

    added some realtime graphs

---
+++ scripts/graph_quaternions.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+rqt_plot \
+	/ik/output/data[0] \
+	/ik/output/data[1] \
+	/ik/output/data[2] \
+	/ik/output/data[3] \
+	/ik/output/data[4] \
+	/ik/output/data[5] \
+	/ik/output/data[6] \
+	/ik/output/data[7] \
+	/ik/output/data[8] \
+	/ik/output/data[9] \
+	/ik/output/data[10] \
+	/ik/output/data[11] \
+	/ik/output/data[12] \
+	/ik/output/data[13] \
+	/ik/output/data[14] \
+	/ik/output/data[15] \
+	/ik/output/data[16] \
+	/ik/output/data[17] 
