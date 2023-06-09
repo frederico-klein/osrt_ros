9104194399a2bc505a1d98fbcf8f24107a062d6c
frederico-klein <frekle@kth.se>
2023-06-09 22:29:53 +0200

    when you try to read out of bounds there was an error message showing. now this is no longer the case

---
+++ scripts/graph_tau_id_1992.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+rqt_plot \
+	/id_node/output/data[0] \
+	/id_node/output/data[1] \
+	/id_node/output/data[2] \
+	/id_node/output/data[3] \
+	/id_node/output/data[4] \
+	/id_node/output/data[5] \
+	/id_node/output/data[6] \
+	/id_node/output/data[7] \
+	/id_node/output/data[8] \
+	/id_node/output/data[9] \
+	/id_node/output/data[10] \
+	/id_node/output/data[11] \
+	/id_node/output/data[12] \
+	/id_node/output/data[13] \
+	/id_node/output/data[14] \
+	/id_node/output/data[15] \
+	/id_node/output/data[16] \
+	/id_node/output/data[17] \
+	/id_node/output/data[18] 
